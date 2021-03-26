9b8a033cc52b550fb7bb06b988df69af89cbc874
Dustin-Har <harbaughdustin@yahoo.com>
2021-03-25 20:21:53 -0400

    Add test for game class

--- test/Deck.-test.js
+++ test/Deck.-test.js
@@ -29,5 +29,14 @@ describe ('Deck', () => {
         const deck = new Deck(array);
 
         assert.equal(deck.cards, array)
+    });
+
+    it('should be able to count cards', () => {
+        const card1 = new Card(1, 'What is Dustin\'s favorite animal', ['sea otter', 'fox', 'capybara'], 'fox');
+        const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const array = [card1, card2];
+        const deck = new Deck(array);
+
+        assert.equal(deck.countCards(), 2);
     })
 })
